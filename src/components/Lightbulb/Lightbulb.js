478be95b2d12224677b0fb526b6d1a912a35f7d5
Shra <shrayswarup@gmail.com>
2023-04-19 14:19:24 -0700

    Update Lightbulb.js

--- src/components/Lightbulb/Lightbulb.js
+++ src/components/Lightbulb/Lightbulb.js
@@ -1,22 +1,22 @@
 import uniqid from 'uniqid'
-import { skills } from '../../portfolio'
+import { lightbulb, skills } from '../../portfolio'
 import './Lightbulb.css'
 
-const Skills = () => {
-  if (!skills.length) return null
+const Lightbulb = () => {
+  if (!lightbulb.status) return null
 
   return (
     <section className="section skills" id="skills">
       <h2 className="section__title">Control my Lamp</h2>
-      <ul className="skills__list">
+      {/* <ul className="skills__list">
         {skills.map((skill) => (
           <li key={uniqid()} className="skills__list-item btn btn--plain">
             {skill}
           </li>
         ))}
-      </ul>
+      </ul> */}
     </section>
   )
 }
 
-export default Skills
+export default Lightbulb
