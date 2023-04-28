3041d055533994b5026df6434da29c5284d672a5
Shra <shrayswarup@gmail.com>
2023-04-28 02:17:11 -0700

    toggle

--- src/components/Lightbulb/Lightbulb.js
+++ src/components/Lightbulb/Lightbulb.js
@@ -1,22 +1,42 @@
-import uniqid from 'uniqid'
-import { lightbulb, skills } from '../../portfolio'
-import './Lightbulb.css'
+
+//   return (
+//     <section className="section skills" id="skills">
+//       <h2 className="section__title">Control my Lamp</h2>
+//       {/* <ul className="skills__list">
+//         {skills.map((skill) => (
+//           <li key={uniqid()} className="skills__list-item btn btn--plain">
+//             {skill}
+//           </li>
+//         ))}
+//       </ul> */}
+//     </section>
+//   )
+// }
+
+// export default Lightbulb
+
+import { lightbulb } from '../../portfolio';
+import './Lightbulb.css';
 
 const Lightbulb = () => {
-  if (!lightbulb.status) return null
+  // if (!lightbulb.status) return null;
+
+  const handleClick = async () => {
+    const response = await fetch('/.netlify/functions/toggle').then(response => response.json())
+    console.log(JSON.stringify(response))
+    // console.log('Button clicked');
+  };
 
   return (
-    <section className="section skills" id="skills">
+    <section className="section lightbulb" id="lightbulb">
       <h2 className="section__title">Control my Lamp</h2>
-      {/* <ul className="skills__list">
-        {skills.map((skill) => (
-          <li key={uniqid()} className="skills__list-item btn btn--plain">
-            {skill}
-          </li>
-        ))}
-      </ul> */}
+      <ul className="lightbulb__list">
+        <li className="lightbulb__list-item btn btn--plain" onClick={handleClick}>
+          Toggle Lamp
+        </li>
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Lightbulb
+export default Lightbulb;
