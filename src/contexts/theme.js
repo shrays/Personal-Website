0af4c8f38c567577002406f0c5f3903c6f1d44fe
Shra <shrayswarup@gmail.com>
2023-04-29 00:14:00 -0700

    Update theme.js

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -32,3 +32,52 @@ ThemeProvider.propTypes = {
 }
 
 export { ThemeProvider, ThemeContext }
+
+/* This implementation saves user's selected theme, otherwise 
+defaulting to os theme */
+
+// import { createContext, useEffect, useState } from 'react'
+// import PropTypes from 'prop-types'
+
+// const ThemeContext = createContext()
+
+// const ThemeProvider = ({ children }) => {
+//   const [themeName, setThemeName] = useState(() => {
+//     const savedTheme = localStorage.getItem('themeName')
+//     if (savedTheme) {
+//       return savedTheme
+//     }
+//     const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+//     return darkMediaQuery.matches ? 'dark' : 'light'
+//   })
+
+//   useEffect(() => {
+//     const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+//     const handleChange = (e) => {
+//       if (!localStorage.getItem('themeName')) {
+//         const updatedTheme = e.matches ? 'dark' : 'light'
+//         setThemeName(updatedTheme)
+//       }
+//     }
+//     darkMediaQuery.addEventListener('change', handleChange)
+//     return () => darkMediaQuery.removeEventListener('change', handleChange)
+//   }, [])
+
+//   const toggleTheme = () => {
+//     const name = themeName === 'dark' ? 'light' : 'dark'
+//     localStorage.setItem('themeName', name)
+//     setThemeName(name)
+//   }
+
+//   return (
+//     <ThemeContext.Provider value={[{ themeName, toggleTheme }]}>
+//       {children}
+//     </ThemeContext.Provider>
+//   )
+// }
+
+// ThemeProvider.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
+
+// export { ThemeProvider, ThemeContext }
