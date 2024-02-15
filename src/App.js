9a792181df8a80602871864e7fbdc0e847cf75ff
Shra <shrayswarup@gmail.com>
2024-02-15 02:39:54 -0700

    Unsafe Area Background (ie Notches)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import React, { useContext, useEffect } from 'react';
 import { ThemeContext } from './contexts/theme'
 import Header from './components/Header/Header'
 import About from './components/About/About'
@@ -11,6 +11,19 @@ import './App.css'
 
 const App = () => {
   const [{ themeName }] = useContext(ThemeContext)
+  // Set background color for unsafe areas (ie notched devices)
+  useEffect(() => {
+    const rootElement = document.getElementById('top');
+    if (rootElement) {
+      const style = window.getComputedStyle(rootElement);
+      const bgColor = style.getPropertyValue('--clr-bg').trim();
+
+      const metaThemeColor = document.querySelector('meta[name=theme-color]');
+      if (metaThemeColor) {
+        metaThemeColor.setAttribute('content', bgColor);
+      }
+    }
+  }, [themeName]); // Re-run the effect when themeName changes
 
   return (
     <div id="top" className={`${themeName} app`}>
